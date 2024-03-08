91b3e8b93deec1a59fe7031a3e5a87fb4c934b7f
Gaurav <gaurav1223344@gmail.com>
2024-03-08 15:21:36 +0530

    project setup ready

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// https://enormous-martin-74.clerk.accounts.dev
+export default {
+    providers: [
+      {
+        domain: "https://enormous-martin-74.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ convex/files.ts
@@ -0,0 +1,35 @@
+// Mutations insert, update and remove data from the database, check authentication or perform other business logic, and optionally return a response to the client application.
+import { ConvexError, v } from 'convex/values'
+import {mutation, query} from './_generated/server'
+
+export const createFile = mutation({
+    args:{
+        name: v.string()
+    },
+    handler: async(ctx,args)=>{
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new ConvexError("You must be logged in to upload file")
+        }
+
+        await ctx.db.insert('files',{
+            name: args.name,
+        })
+    }
+})
+
+
+export const getFiles = query({
+    args:{},
+    handler: async(ctx,args)=>{
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            return []
+        }
+        return await ctx.db.query("files").collect()
+    }
+})
+
+ 
